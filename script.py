1694ce03113b9225e1123bcebdad81a7f8ea2d5f
BK-Modding <kavesbteja@gmail.com>
2019-03-29 12:10:34 +0530

    Fixed more bugs

--- script.py
+++ script.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from sklearn.metrics import r2_score
 
-import sympy as sym
+useMeasuredTemp = True
 
 def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
+def func2(x, a, b):
+    return a * np.exp(-b * x) + 27.7
+
 data = pd.read_csv('brominewater-glycerol.csv')
 data2 = pd.read_csv('glycerol-ethanol.csv')
 
-option = 'pair2'
+option = 'pair1'
 regression = True
 
 if option == 'pair1':
@@ -29,14 +32,25 @@ if option == 'pair1':
         y1 = np.array(temp1, dtype=float)
         y2 = np.array(temp2, dtype=float)
 
-        popt1, pcov1 = curve_fit(func, x, y1)
-        print("Glycerol: params = {}, R^2 = {}".format(popt1, r2_score(y1, func(x, *popt1))))
+        if useMeasuredTemp:
+            popt1, pcov1 = curve_fit(func2, x, y1)
+            print("Glycerol: params = {}, R^2 = {}".format(popt1, r2_score(y1, func2(x, *popt1))))
 
-        popt2, pcov2 = curve_fit(func, x, y2)
-        print("Bromine water: params = {}, R^2 = {}".format(popt2, r2_score(y2, func(x, *popt1))))
+            popt2, pcov2 = curve_fit(func2, x, y2)
+            print("Bromine water: params = {}, R^2 = {}".format(popt2, r2_score(y2, func2(x, *popt2))))
 
-        plt.plot(x, func(x, *popt1), 'r-', label="glycerol fitted curve")
-        plt.plot(x, func(x, *popt2), 'g-', label="bromine water fitted curve")
+            plt.plot(x, func2(x, *popt1), 'r-', label="glycerol fitted curve")
+            plt.plot(x, func2(x, *popt2), 'g-', label="bromine water fitted curve")
+        else:
+            popt1, pcov1 = curve_fit(func, x, y1)
+            print("Glycerol: params = {}, R^2 = {}".format(popt1, r2_score(y1, func(x, *popt1))))
+
+            popt2, pcov2 = curve_fit(func, x, y2)
+            print("Bromine water: params = {}, R^2 = {}".format(popt2, r2_score(y2, func(x, *popt2))))
+
+            plt.plot(x, func(x, *popt1), 'r-', label="glycerol fitted curve")
+            plt.plot(x, func(x, *popt2), 'g-', label="bromine water fitted curve")
+
 
     plt.legend(loc='upper right')
     plt.show()
@@ -54,14 +68,24 @@ elif option == 'pair2':
         y1 = np.array(temp1, dtype=float)
         y2 = np.array(temp2, dtype=float)
 
-        popt1, pcov1 = curve_fit(func, x, y1)
-        print("Glycerol: params = {}, R^2 = {}".format(popt1, r2_score(y1, func(x, *popt1))))
+        if useMeasuredTemp:
+            popt1, pcov1 = curve_fit(func2, x, y1)
+            print("Ethanol: params = {}, R^2 = {}".format(popt1, r2_score(y1, func2(x, *popt1))))
 
-        popt2, pcov2 = curve_fit(func, x, y2)
-        print("Bromine water: params = {}, R^2 = {}".format(popt2, r2_score(y2, func(x, *popt1))))
+            popt2, pcov2 = curve_fit(func2, x, y2)
+            print("Glycerol: params = {}, R^2 = {}".format(popt2, r2_score(y2, func2(x, *popt2))))
 
-        plt.plot(x, func(x, *popt1), 'r-', label="ethanol fitted curve")
-        plt.plot(x, func(x, *popt2), 'g-', label="glycerol fitted curve")
+            plt.plot(x, func2(x, *popt1), 'r-', label="ethanol fitted curve")
+            plt.plot(x, func2(x, *popt2), 'g-', label="glycerol fitted curve")
+        else:
+            popt1, pcov1 = curve_fit(func, x, y1)
+            print("Ethanol: params = {}, R^2 = {}".format(popt1, r2_score(y1, func(x, *popt1))))
+
+            popt2, pcov2 = curve_fit(func, x, y2)
+            print("Glycerol: params = {}, R^2 = {}".format(popt2, r2_score(y2, func(x, *popt2))))
+
+            plt.plot(x, func(x, *popt1), 'r-', label="ethanol fitted curve")
+            plt.plot(x, func(x, *popt2), 'g-', label="glycerol fitted curve")
 
     plt.legend(loc='upper right')
     plt.show()
